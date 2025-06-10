5091f6549eddcee1c43896fcf2ca7571fa911238
Javed Shamma <javedshamma@gmail.com>
2025-06-11 00:41:09 +0530

    initial

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const serverless = require('serverless-http');
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Test route
+// Routes
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to QR Scanner Backend API djhdjs' });
 });
-app.get('/bins',(req,res)=>{
-  
-  res.json({message:"BIN80046001"})
-})
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});    
+
+app.get('/bins', (req, res) => {
+  res.json({ message: 'BIN80046001' });
+});
+
+// Export as serverless function
+module.exports = serverless(app);
